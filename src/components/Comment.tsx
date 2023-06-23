103828f89bf00886ed2e618c9a03d4f55493395a
Wellington dos Santos Silva <106429237+wffwds@users.noreply.github.com>
2023-06-22 21:52:06 -0400

    Adicionado TypeScript ao projeto 01-fundamentos

---
+++ src/components/Comment.tsx
@@ -0,0 +1,50 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+import { useState } from 'react'
+
+interface CommentProps {
+  content: string
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCont] = useState(0)
+  function handleDeleteComment(){
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment(){
+    setLikeCont((state) => {
+      return state + 1
+    })
+  }
+  return(
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false} src="http://github.com/diego3g.png" 
+        alt=''
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+              <div className={styles.authorAndTime}>
+                <strong>Diego Fernandes</strong>
+                <time title="18 de junho às 13:49h" dateTime="2023-06-18 14:09:34">Cerca de 1h atrás</time>
+              </div>
+              <button onClick={handleDeleteComment} title="Deletar comentário">
+                <Trash size={24}/>
+              </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer onClick={handleLikeComment}>
+          <button>
+          <ThumbsUp />
+          Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from './Sidebar.module.css'
+import { Avatar } from "./Avatar";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60"
+      />
+      <div className={styles.profile}>
+        <Avatar src="http://github.com/wffwds.png" />
+        <strong>Wellington dos Santos Silva</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+)
+}
